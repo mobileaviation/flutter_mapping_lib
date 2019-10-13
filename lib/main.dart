e11bebdfb498fa991c6237f8aa7770dc4248f0ec
Mobile Aviation Tools <rob@robenanita.nl>
2019-10-13 21:21:29 +0200

    Overlay tests on real device

--- lib/main.dart
+++ lib/main.dart
@@ -161,9 +161,11 @@ class MyApp extends StatelessWidget {
     double east = 5.717278709730119;
     double west = 5.378079539513956;
 
-    paths.getExternalStorageDirectory().then((value) {
-      log("Apps Documents dir: " + value.path);
-      String fileEHLE = value.path + "/" + "EH-AD-2.EHLE-VAC.png";
+    //paths.getExternalStorageDirectory().then((value) {
+
+      String path = "/sdcard/Download";
+      log("Apps Documents dir: " + path);
+      String fileEHLE = path + "/" + "EH-AD-2.EHLE-VAC.png";
       OverlayImage ehleImage = OverlayImage(File(fileEHLE));
       ehleImage.setImageBox(north, south, west, east);
 
@@ -171,7 +173,7 @@ class MyApp extends StatelessWidget {
       overlayLayer.AddImage(ehleImage);
       mapView.AddLayer(overlayLayer);
 
-    });
+    //});
 
   }
 
